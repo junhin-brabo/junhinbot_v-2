ed6249f93fabec07e80edda6ad779331c103944c
junhin-brabo <mitodoclash3@gmail.com>
2022-02-22 19:55:18 -0300

    first commit

---
+++ src/aluguel.js
@@ -0,0 +1,29 @@
+const aluguel = () => { 
+	return `           
+╔══✪〘 ALUGUEL BOT 〙✪══
+╠═════════════════════
+║✔ *LISTA DE ALUGUEL E CRIAR BOTS:*
+║🏅 *SE TORNA VIP 25 R$*
+║🏅 *ALUGUEL: 30 / GRUPO (MÊS)*
+║🏅 *COMPRA BASE EDITAVEL: 80 (PODE SER PROPRIETÁRIO)*
+║✔ *PODE PAGAR ATRAVÉS DE: PIX, MERCADO PAGO, BOLETO*
+╠═════════════════════
+║*VANTAGENS?*
+║🔎 *veja com wa.me/557187566648*
+╠═════════════════════
+║ *𝙇𝙞𝙣𝙠 𝙙𝙤 𝙘𝙝𝙖𝙩 𝙙𝙤 𝙟𝙪𝙣𝙝𝙞𝙣*
+║ ⇲    
+║    https://chat.whatsapp.comGaxOW8aZxAeCP5xJrBhBTq
+╠═════════════════════
+║ *𝙇𝙞𝙣𝙠 𝙙𝙤 𝙘𝙝𝙖𝙩 𝙙𝙤 𝙟𝙪𝙣𝙝𝙞𝙣 [ 2 ]*
+║ ⇲    
+║    https://chat.whatsapp.com/D7LAWs0rRUR8Yr09KfLs72
+╠═════════════════════
+║ *𝙇𝙞𝙣𝙠 𝙙𝙤 𝙘𝙝𝙖𝙩 𝙙𝙤 𝙟𝙪𝙣𝙝𝙞𝙣 [ 3 ]*
+║ ⇲    
+║    https://chat.whatsapp.com/C3ZPDQYwTB0GacJrVgskbM
+║   
+╚═✪〘 JUNHIN BOT 〙✪══
+`
+}
+exports.aluguel = aluguel
---
+++ src/daftarvip.js
@@ -0,0 +1,8 @@
+const daftarvip = (prefix) => { 
+	return `
+
+*Caso tenha interesse em aderir premium contate a Junhin.*
+
+https://seulink.online/Junhinbot_Premium `
+}
+exports.daftarvip = daftarvip
